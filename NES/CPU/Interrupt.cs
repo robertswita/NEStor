aa98e48ed8805f5e0f7736a0f7e601279f14b71c
Mateusz Kosikowski, PhD <mateusz.kosikowski@gmail.com>
2025-06-08 19:19:42 +0100

    Decouple CPU from Interrupt (#3)
    * Moved Files Around
    
    * Decouple CPU from Interrupt
    
    * Added ICycleProviding protocol

---
+++ NES/CPU/Interrupt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace NES
+{
+    public interface ICycleProviding
+    {
+        int Cycle { get; set; }
+    }
+    public interface IInterrupt: ICycleProviding
+    {
+        bool IsActive { get; }
+        bool IsReady { get; }
+        bool Enabled { get; set; }
+        void Start(int delay = 1);
+        void Acknowledge();
+        void Delay();
+    }
+
+    internal class Interrupt: IInterrupt
+    {
+        ICycleProviding CycleProvider;
+        internal int delay = 1;
+        public int Cycle { get; set; }
+        public bool Enabled { get; set; }
+        int Eta { get { return CycleProvider.Cycle - Cycle - delay; } }
+        public bool IsActive { get { return Enabled && Eta >= 0; } }
+        public bool IsReady { get { return Enabled && Eta == 0; } }
+
+        public void Start(int delay = 1)
+        {
+            if (!Enabled)
+            {
+                Enabled = true;
+                Cycle = CycleProvider.Cycle;
+                this.delay = delay;
+            }
+        }
+        public void Acknowledge()
+        {
+            Enabled = false;
+            //Delay = 1;
+        }
+        public Interrupt(ICycleProviding cycleProvider)
+        {
+            CycleProvider = cycleProvider;
+        }
+        public void Delay()
+        {
+            delay++;
+        }
+    }
+}
