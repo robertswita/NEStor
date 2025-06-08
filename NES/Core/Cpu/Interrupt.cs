650a658f54e54d22652ce18da75a2c63325f3686
Mateusz Kosikowski, PhD <mateusz.kosikowski@gmail.com>
2025-06-08 20:34:21 +0100

    fixed after merging origin/main

--- NES/Core/Cpu/Interrupt.cs
+++ NES/Core/Cpu/Interrupt.cs
@@ -2,15 +2,29 @@
 
 namespace NEStor.Core.Cpu
 {
-    internal class Interrupt
+    public interface ICycleProviding
     {
-        CPU Cpu;
-        public bool Enabled;
-        public int Delay = 1;
-        public int Cycle;
+        int Cycle { get; set; }
+    }
+    public interface IInterrupt : ICycleProviding
+    {
+        bool IsActive { get; }
+        bool IsReady { get; }
+        bool Enabled { get; set; }
+        int Delay { get; set; }
+        int CyclesPassed { get; }
+        void Start(int delay = 1);
+        void Acknowledge();
+    }
+    internal class Interrupt: IInterrupt
+    {
+        ICycleProviding cycleProvider;
+        public bool Enabled { get; set; } = false; 
+        public int Delay { get; set; } = 1;
+        public int Cycle { get; set; } = 0;
 
         //public int Eta { get { return Cpu.Cycle - Cycle - Delay; } }
-        public int CyclesPassed { get { return Cpu.Cycle - Cycle; } }
+        public int CyclesPassed { get { return cycleProvider.Cycle - Cycle; } }
         public bool IsActive { get { return Enabled && CyclesPassed >= Delay; } }
         public bool IsReady { get { return Enabled && CyclesPassed == Delay; } }
         public void Start(int delay = 1) 
@@ -18,7 +32,7 @@ namespace NEStor.Core.Cpu
             if (!Enabled)
             {
                 Enabled = true;
-                Cycle = Cpu.Cycle;
+                Cycle = cycleProvider.Cycle;
                 Delay = delay;
             }
         }
@@ -27,10 +41,9 @@ namespace NEStor.Core.Cpu
             Enabled = false;
             //Delay = 1;
         }
-        public Interrupt(CPU cpu)
+        public Interrupt(ICycleProviding cycleProvider)
         {
-            Cpu = cpu;
-            Cpu.Interrupts.Add(this);
+            this.cycleProvider = cycleProvider;
         }
     }
 }
